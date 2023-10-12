f25b113da437d43a87ff5e2b395474c9a92d443a
brunoribeiro4488 <118730749+brunoribeiro4488@users.noreply.github.com>
2023-10-12 21:04:05 +0100

    Background diferente para post do dia
    Alteração da cor do background dos posts do dia atual

--- public/app/src/modules/forum/components/posts/post/components/PostMeta.tsx
+++ public/app/src/modules/forum/components/posts/post/components/PostMeta.tsx
@@ -7,10 +7,20 @@ import "../styles/PostMeta.sass"
 
 interface PostMetaProps extends Post {
   includeLink?: boolean;
+  page?: string;
+}
+
+function todaysPosts(date: Date | String){
+    if(date == undefined){
+        return false;
+    }
+    let today  = new Date().toLocaleDateString("en-US");
+    let data = new Date(date.toString()).toLocaleDateString("en-US");
+    return today == data;
 }
 
 const PostMeta: React.FC<PostMetaProps> = (props) => (
-  <div className="post-row-content">
+    <div className={`post-row-content ${todaysPosts(props.createdAt) && props.page == "NEW" ? "red-background": ""}`}>
     {props.includeLink === false ? '' : <Link to={`/discuss/${props.slug}`} className="title">"{props.title}" {props.link ? <span className="link">[link]</span> : ''}</Link>}
     <div className="post-row-meta">
       {moment(props.createdAt).fromNow()} | {`by `} <Link to={`/member/${props.postAuthor}`}>{props.postAuthor}</Link> | {`${props.numComments} comments`}
